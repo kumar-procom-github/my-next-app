8c322d98074eb7e55bcf46026efc0d307837ab77
Kumar Ch <kumarch@Kumars-MacBook-Air.local>
2025-06-16 15:50:59 +0530

    Your message here

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,6 +4,7 @@ export default function Contact() {
       <main className="p-8">
         <h1 className="text-3xl font-bold">Contact Page</h1>
         <p className="text-gray-700">This is my Second Next.js page.</p>
+        <p>After deplying to vercel this page is updated.</p>
       </main>
     );
   }
