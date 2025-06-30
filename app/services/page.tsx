faf3b1c91cbca8ffc8162675972aaee3a2df0609
Kumar Ch <kumarch@Kumars-MacBook-Air.local>
2025-06-30 09:59:29 +0530

    new changes added.

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Services() {
+    return (
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Services Page</h1>
+        <p className="text-gray-700">This is my Second Next.js page.</p>
+        <p>After deplying to vercel this page is updated.</p>
+      </main>
+    );
+  }
+  
